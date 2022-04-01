10f3c214d49f5f7310e7f7bd60163191f9c76fbc
fansters <edmundseizentals@gmail.com>
2022-04-01 16:37:07 +0300

    added burger menu functionality, removed old js

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,9 @@
 
-const burgerMenu = document.querySelector('.header__burgerMenu');
-const burgerBtn = document.querySelector('.header__hamburger');
-const closeBtn = document.querySelector('.header__close');
+// const burgerMenu = document.querySelector('.header__burgerMenu');
+// const burgerBtn = document.querySelector('.header__hamburger');
+// const closeBtn = document.querySelector('.header__close');
 
-burgerMenu.addEventListener('click', function () {
-   burgerBtn.classList.toggle('hidden');
-   closeBtn.classList.toggle('hidden');
-})
+// burgerMenu.addEventListener('click', function () {
+//    burgerBtn.classList.toggle('hidden');
+//    closeBtn.classList.toggle('hidden');
+// })
